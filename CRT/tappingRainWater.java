dedef6fcb2c51337e2b44188591d5dbfe1d571fc
Ankit Ojha <Ankitojha121212@gmail.com>
2025-02-12 18:39:15 +0530

    work on react basics

---
+++ CRT/tappingRainWater.java
@@ -0,0 +1,6 @@
+public class tappingRainWater {
+    public static void main(String[] args) {
+        int arr[] = { 3, 0, 1, 0, 4, 0, 2 };
+
+    }
+}
---
+++ firstpro/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hii</h1>
+    </>
+  )
+}
+
+export default App
