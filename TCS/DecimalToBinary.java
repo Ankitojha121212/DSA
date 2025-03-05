42bbf6654c6580c9faf4d5708103b41177e12cd3
Ankit Ojha <Ankitojha121212@gmail.com>
2025-03-05 21:58:16 +0530

    Today i have to solve 20 basic problems

--- TCS/DecimalToBinary.java
+++ TCS/DecimalToBinary.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class DecimalToBinary {
     public static void DtoB(int n) {
-        int[] arr = new int[20];
+        if (n <= 0) {
+            System.out.print("0");
+            return;
+        }
+        int[] arr = new int[32];
         int count = 0;
         while (n > 0) {
-            int res = n % 2;
-            n = n / 2;
-            arr[count] = res;
+            arr[count] = n % 2;
             count++;
+            n = n / 2;
+        }
+
+        for (int j = count - 1; j >= 0; j--) {
+            System.out.print(arr[j] + " ");
         }
 
     }
